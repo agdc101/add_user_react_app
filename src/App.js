f94447427c5d21e531bbd8de9feee208f6475286
agdc101 <agdc101@gmail.com>
2022-10-17 17:53:05 +0100

    dynamic user adding

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import AddUser from './components/Users/AddUser';
 import Card from './components/UI/Card';
 import styles from '../src/components/Users/AddUser.module.css';
+import UsersList from './components/Users/UsersList';
 
 function App() {
+  const [usersList, setUsersList] = useState(['']);
+
+  const addUser = (newName, newAge) => {
+    setUsersList((prevUsersList) => {
+      return [...prevUsersList, { name: newName, age: newAge }]
+    });
+  };
+
   return (
     <Card className={styles.input}>
-      <AddUser/>
+      <AddUser onAddUser={addUser}/>
+      <UsersList users={usersList}/>
     </Card>
   );
 }
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "../Button/Button";
 
 const AddUser = props => {
+    const [userName, setUserName] = useState('');
+    const [userAge, setUserAge] = useState('');
+
     const AddUserHandler = (e) => {
         e.preventDefault();
+        if ((userName.trim().length !== 0 || userAge.trim().length !== 0) && (+userAge > 1)) {
+            props.onAddUser(userName, userAge);
+            setUserName('');
+            setUserAge('');
+        }
+    }
+
+    const usernameChange = (e) => {
+        setUserName(e.target.value);
+    }
+
+    const ageChange = (e) => {
+        setUserAge(e.target.value);
     }
 
     return (
         <form onSubmit ={AddUserHandler}>
             <label htmlFor="username">Username</label>
-            <input id="username" type="text"></input>
+            <input id="username" type="text" onChange={usernameChange} value={userName}></input>
             <label htmlFor="age">Age</label>
-            <input id="age" type="number"></input>
-            <button type="submit">Add User</button>
+            <input id="age" type="number" onChange={ageChange} value={userAge}></input>
+            <Button type="submit">Submit</Button>
         </form>
     )
 };
