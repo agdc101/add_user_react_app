3900ec71f33ee6a721085d258e7d7716c951b5a6
agdc101 <agdc101@gmail.com>
2022-10-25 10:23:19 +0100

    error modal added

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import Button from "../Button/Button";
+import ErrorModal from "../UI/ErrorModal";
 
 const AddUser = props => {
     const [userName, setUserName] = useState('');
     const [userAge, setUserAge] = useState('');
+    const [error, setError] = useState('');
 
     const AddUserHandler = (e) => {
         e.preventDefault();
@@ -11,6 +13,11 @@ const AddUser = props => {
             props.onAddUser(userName, userAge);
             setUserName('');
             setUserAge('');
+        } else {
+            setError({
+                title: 'Invalid input',
+                message: 'Please enter a valid name and age'
+            })
         }
     }
 
@@ -22,14 +29,21 @@ const AddUser = props => {
         setUserAge(e.target.value);
     }
 
+    const errorHandler = () => {
+        setError(null);
+    }
+
     return (
-        <form onSubmit ={AddUserHandler}>
-            <label htmlFor="username">Username</label>
-            <input id="username" type="text" onChange={usernameChange} value={userName}></input>
-            <label htmlFor="age">Age</label>
-            <input id="age" type="number" onChange={ageChange} value={userAge}></input>
-            <Button type="submit">Submit</Button>
-        </form>
+        <div>
+            {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
+            <form onSubmit ={AddUserHandler}>
+                <label htmlFor="username">Username</label>
+                <input id="username" type="text" onChange={usernameChange} value={userName}></input>
+                <label htmlFor="age">Age</label>
+                <input id="age" type="number" onChange={ageChange} value={userAge}></input>
+                <Button type="submit">Submit</Button>
+            </form>
+        </div>
     )
 };
 
